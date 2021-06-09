d4b49a2e67d990d4b118c9f904d656da2a100ec6
EDMONDGIHOZO <gihozoedmond@gmail.com>
2021-06-09 16:30:47 +0200

    configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const { colors } = require("tailwindcss/defaultTheme");
 
 module.exports = {
+  mode: "jit",
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
